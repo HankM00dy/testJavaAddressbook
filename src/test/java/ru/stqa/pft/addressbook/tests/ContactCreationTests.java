fcf569f1292a8e7ba8e8df65289dbeb14caf3aa7
Daniel Sedin <skrat_57@mail.ru>
2021-08-25 16:32:38 +0300

    Запуск тестов через maven + testNg через командную строку и команду с ключом

--- src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,12 +5,27 @@ import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test(enabled = false)
+    @Test()
     public void testContactCreation() {
         app.goTo().goToHomePage();
         app.getContactHelper().initContactCreation();
         app.getContactHelper().fillContactForm(new ContactData("test_name", "test_surname", "test1"), true);
         app.getContactHelper().submitContactCreation();
         app.getContactHelper().returnToHomePage();
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("Test1 success");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Test2 success");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("Test3 success");
     }
 }
